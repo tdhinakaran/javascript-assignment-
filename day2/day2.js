df70927926aae075f0b4558d61428cd711fa04ee
tdhinakaran <83864192+tdhinakaran@users.noreply.github.com>
2021-08-06 09:10:26 +0530

    Assignment Day2

---
+++ day2/day2.js
@@ -0,0 +1,14 @@
+let colorsArr = ["Blue", "White", "Black", "Green", "Red"];
+
+let i = colorsArr.length - 1;
+
+while (i >= 0) {
+    console.log(colorsArr[i]);
+    i--
+}
+
+for (let i = 1; i <= 50; i++) {
+    if (i % 5 == 0) {
+        console.log(i);
+    }
+}
